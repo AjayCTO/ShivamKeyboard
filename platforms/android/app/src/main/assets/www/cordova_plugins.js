643c324cb12c0f582108877a3b485dcf7f5508c7
Ajay Paliwal <ajaypaliwal.cto@outlook.com>
2019-05-15 12:30:16 +0530

    Commit
    Commit

---
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,16 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-ionic-keyboard.keyboard",
+      "file": "plugins/cordova-plugin-ionic-keyboard/www/android/keyboard.js",
+      "pluginId": "cordova-plugin-ionic-keyboard",
+      "clobbers": [
+        "window.Keyboard"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-ionic-keyboard": "2.1.3",
+    "cordova-plugin-whitelist": "1.3.3"
+  };
+});
